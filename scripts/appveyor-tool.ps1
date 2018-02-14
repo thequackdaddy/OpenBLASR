63d37ef29386154d803c333804a888b8a9ab9f47
thequackdaddy <pquack@gmail.com>
2018-02-14 09:32:06 -0600

    Initial Commit

---
+++ scripts/appveyor-tool.ps1
@@ -0,0 +1,81 @@
+# https://github.com/krlmlr/r-appveyor/tree/master/scripts
+
+$CRAN = "https://cloud.r-project.org"
+
+# Found at http://zduck.com/2012/powershell-batch-files-exit-codes/
+Function Exec
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Position=0, Mandatory=1)]
+        [scriptblock]$Command,
+        [Parameter(Position=1, Mandatory=0)]
+        [string]$ErrorMessage = "Execution of command failed.`n$Command"
+    )
+    $ErrorActionPreference = "Continue"
+    & $Command 2>&1 | %{ "$_" }
+    if ($LastExitCode -ne 0) {
+        throw "Exec: $ErrorMessage`nExit code: $LastExitCode"
+    }
+}
+
+Function Progress
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Position=0, Mandatory=0)]
+        [string]$Message = ""
+    )
+
+    $ProgressMessage = '== ' + (Get-Date) + ': ' + $Message
+
+    Write-Host $ProgressMessage -ForegroundColor Magenta
+}
+
+Function InstallRtools {
+  if ( -not(Test-Path Env:\RTOOLS_VERSION) ) {
+    Progress "Determining Rtools version"
+    $rtoolsver = $(Invoke-WebRequest ($CRAN + "/bin/windows/Rtools/VERSION.txt")).Content.Split(' ')[2].Split('.')[0..1] -Join ''
+  }
+  Else {
+    $rtoolsver = $env:RTOOLS_VERSION
+  }
+
+  $rtoolsurl = $CRAN + "/bin/windows/Rtools/Rtools$rtoolsver.exe"
+
+  Progress ("Downloading Rtools from: " + $rtoolsurl)
+  & "C:\Program Files\Git\mingw64\bin\curl.exe" -s -o ../Rtools-current.exe -L $rtoolsurl
+
+  Progress "Running Rtools installer"
+  Start-Process -FilePath ..\Rtools-current.exe -ArgumentList /VERYSILENT -NoNewWindow -Wait
+
+  $RtoolsDrive = "C:"
+  echo "Rtools is now available on drive $RtoolsDrive"
+
+  Progress "Setting PATH"
+  if ( -not(Test-Path Env:\GCC_PATH) ) {
+    $gcc_path = "gcc-4.6.3"
+  }
+  Else {
+    $gcc_path = $env:GCC_PATH
+  }
+  $env:PATH = $RtoolsDrive + '\Rtools\bin;' + $RtoolsDrive + '\Rtools\mingw_$(WIN)\bin;' + $env:PATH
+  $env:BINPREF=$RtoolsDrive + '/Rtools/mingw_$(WIN)/bin/'
+}
+
+Function Bootstrap {
+  [CmdletBinding()]
+  Param()
+
+  Progress "Bootstrap: Start"
+
+  Progress "Adding MSYS2 tools to PATH"
+  $env:PATH = "C:\msys64\usr\bin;" + $env:PATH
+
+  Progress "Setting time zone"
+  tzutil /g
+  tzutil /s "GMT Standard Time"
+  tzutil /g
+
+InstallRtools
+}
