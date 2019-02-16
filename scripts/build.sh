aed3973055e606a4ca3c3a0e90cb1aedc9b5d8cc
thequackdaddy <pquack@gmail.com>
2019-02-16 17:34:17 -0600

    aliasing

--- scripts/build.sh
+++ scripts/build.sh
@@ -17,6 +17,10 @@ ls -lash
 cd /c/projects/openblasr/OpenBLAS
 pwd
 
+alias gcc=x86_64-pc-msys-gcc
+alias gfortran=x86_64-pc-msys-gfortran
+alias x86_64-pc-msys-ar=x86_64-pc-msys-gcc-ar
+
 export PATH=/mingw64/bin:/mingw32/bin:/usr/bin:/bin
 
 # export PATH=/C/Rtools/mingw_64/bin:/C/Rtools/bin:/usr/bin:/bin
